9e1922d8ab96a20851e1b58271d33dba7f7eef02
qinghe.wen <qinghe.wen@upai.com>
2020-06-29 17:27:38 +0800

    修复中文路径404

--- src/main/java/com/upyun/AllMethodsDemo.java
+++ src/main/java/com/upyun/AllMethodsDemo.java
@@ -7,16 +7,17 @@ import okhttp3.Response;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URLDecoder;
 import java.util.*;
 
 public class AllMethodsDemo {
     public static void main(String[] args) throws Exception {
-        RestManager manager = new RestManager("服务名", "操作员", "操作员密码");
+        RestManager manager = new RestManager("服务名", "操作员账号", "操作员密码");
         String dirUri = "/"; //目录路径
-        String uri = "/";  //目标路径    /<save_as_dir>
-        String source = "/"; //源文件  /<source_to_dir>
+//        String source = "/"; //源文件  /<source_to_dir>
+//        String uri = "/";  //目标路径    /<save_as_dir>
 
-//        toGetFilesList(dirUri,manager); //统计目录下的文件和目录
+//        toGetFilesList(dirUri,manager); //统计目录下的文件和目录 + 数量
 //        deleteFiles(dirUri,manager); //删除目录下的文件
 //        copyDir(uri,source,manager); //复制文件夹
 //        moveDir(uri,source,manager); //移动文件夹
@@ -61,11 +62,11 @@ public class AllMethodsDemo {
      */
     private static void copyDir(String uri, String source, RestManager manager) throws Exception {
         TotalFile totalFile = FileList.getList(source, manager);
-        List list = totalFile.getFileList();
+        List<String> fileList = totalFile.getFileList();
         String bucketName = manager.bucketName;
-        for (int i = 0; i < list.size(); i++) {
-            String sourceFileUri = "/" + bucketName + list.get(i);
-            String toFileUri = "/" + bucketName + uri + list.get(i);
+        for (String filePath : fileList) {
+            String sourceFileUri = "/" + bucketName + URLDecoder.decode(filePath,"UTF-8");
+            String toFileUri = "/" + bucketName + URLDecoder.decode(uri + filePath,"UTF-8");
             Integer integer = MFCopy.copyFile(toFileUri, sourceFileUri, manager.userName, manager.password);
             System.out.println("Source：" + sourceFileUri + "  CopyTo：" + toFileUri + "   Code：" + integer);
         }
@@ -80,11 +81,11 @@ public class AllMethodsDemo {
      */
     private static void moveDir(String uri, String source, RestManager manager) throws Exception {
         TotalFile totalFile = FileList.getList(source, manager);
-        List list = totalFile.getFileList();
+        List<String> fileList = totalFile.getFileList();
         String bucketName = manager.bucketName;
-        for (int i = 0; i < list.size(); i++) {
-            String sourceFileUri = "/" + bucketName + list.get(i);
-            String toFileUri = "/" + bucketName + uri + list.get(i);
+        for (String filePath : fileList) {
+            String sourceFileUri = "/" + bucketName + URLDecoder.decode(filePath,"UTF-8");
+            String toFileUri = "/" + bucketName + URLDecoder.decode(uri + filePath,"UTF-8");
             Integer integer = MFCopy.moveFile(toFileUri, sourceFileUri, manager.userName, manager.password);
             System.out.println("Source：" + sourceFileUri + "  MoveTo：" + toFileUri + "   Code：" + integer);
         }
--- src/main/java/com/upyun/utils/MFCopy.java
+++ src/main/java/com/upyun/utils/MFCopy.java
@@ -1,17 +1,11 @@
 package com.upyun.utils;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+import com.upyun.UpYunUtils;
+
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
 import java.text.SimpleDateFormat;
-import java.util.Base64;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -20,7 +14,6 @@ import java.util.TimeZone;
  * 同服务下复制或移动文件
  */
 public class MFCopy {
-    private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
     private final static String host = "http://v0.api.upyun.com";
 
     public static Integer copyFile(String uri, String source, String operator, String secret) throws Exception {
@@ -30,7 +23,7 @@ public class MFCopy {
         conn.setRequestMethod("PUT");
         conn.setUseCaches(false);
         conn.setDoOutput(true);
-        conn.setRequestProperty("Authorization", getAuth(operator, secret, "PUT", uri, date));
+        conn.setRequestProperty("Authorization", UpYunUtils.sign("PUT", date, uri, operator, secret,""));
         conn.setRequestProperty("X-Upyun-Copy-Source", source);
         conn.setRequestProperty("date", date);
         conn.setRequestProperty("Content-Length", "0");
@@ -58,7 +51,7 @@ public class MFCopy {
         conn.setRequestMethod("PUT");
         conn.setUseCaches(false);
         conn.setDoOutput(true);
-        conn.setRequestProperty("Authorization", getAuth(operator, secret, "PUT", uri, date));
+        conn.setRequestProperty("Authorization", UpYunUtils.sign("PUT",date,uri,operator,secret,""));
         conn.setRequestProperty("X-Upyun-Move-Source", source);
         conn.setRequestProperty("date", date);
         conn.setRequestProperty("Content-Length", "0");
@@ -77,63 +70,6 @@ public class MFCopy {
         }
 
         return responseCode;
-    }
-
-    /**
-     * 获取签名
-     *
-     * @param operator
-     * @param secret
-     * @param method
-     * @param uri
-     * @param date
-     * @return 签名
-     */
-    private static String getAuth(String operator, String secret, String method, String uri, String date) throws Exception {
-        //传入的secret为MD5后的值
-        return sign(operator, secret, method, uri, date, "", "");
-    }
-
-    private static String md5(String string) {
-        byte[] hash;
-        try {
-            hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("UTF-8 is unsupported", e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("MessageDigest不支持MD5Util", e);
-        }
-        StringBuilder hex = new StringBuilder(hash.length * 2);
-        for (byte b : hash) {
-            if ((b & 0xFF) < 0x10) hex.append("0");
-            hex.append(Integer.toHexString(b & 0xFF));
-        }
-        return hex.toString();
-    }
-
-    private static byte[] hashHmac(String data, String key)
-            throws SignatureException, NoSuchAlgorithmException, InvalidKeyException {
-        SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), HMAC_SHA1_ALGORITHM);
-        Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
-        mac.init(signingKey);
-        return mac.doFinal(data.getBytes());
-    }
-
-    public static String sign(String key, String secret, String method, String uri, String date, String policy,
-                              String md5) throws Exception {
-        String value = method + "&" + uri;
-        if (date != "") {
-            value = value + "&" + date;
-        }
-        if (policy != "") {
-            value = value + "&" + policy;
-        }
-        if (md5 != "") {
-            value = value + "&" + md5;
-        }
-        byte[] hmac = hashHmac(value, secret);
-        String sign = Base64.getEncoder().encodeToString(hmac);
-        return "UPYUN " + key + ":" + sign;
     }
 
     public static String getRfc1123Time() {
